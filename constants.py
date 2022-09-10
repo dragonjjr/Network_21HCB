73d0899be3523352e9dba2e3c0bbb0b5d4505d23
Tinh Phan Van <tinh.pv@cep.org.vn>
2022-09-10 19:30:30 +0700

    Maintest

--- constants.py
+++ constants.py
@@ -1,3 +1,4 @@
+APP_REQ = '[RFC822]'
 SMTP_HOST = 'smtp.gmail.com'
 IMAP_HOST = 'imap.gmail.com'
 REMOTE_MAIL = '21HCb.21424057@gmail.com'
---
+++ remote_control.py
@@ -0,0 +1,114 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import QThread, pyqtSignal, QObject
+
+from mail_service import MailService
+
+import global_variables
+
+from helpers import *
+from thread_targets import *
+import app_logging as logging
+
+class LoginThread(QObject):
+    ok = pyqtSignal(MailService)
+    fail = pyqtSignal()
+    finished = pyqtSignal()
+
+    def __init__(self, host_mail: MailService):
+        super(LoginThread, self).__init__()
+        self.host_mail = host_mail
+
+    def start(self):
+        print('Login to mail server...')
+        logging.log('Login to mail server...')
+        status = self.host_mail.login(REMOTE_MAIL, REMOTE_PWD)
+        
+        if status:
+            print('Login successfully')
+            logging.log('Login successfully')
+            self.ok.emit(self.host_mail)
+        else:
+            print('Failed to login with login with name: ' + REMOTE_MAIL)                
+            logging.log(f'Failed to login with login with name: {REMOTE_MAIL}')
+            self.fail.emit()
+        
+        self.finished.emit()
+
+class RemoteControl():
+    def __init__(self):
+        self.app = QApplication(sys.argv)
+        QApplication.setQuitOnLastWindowClosed(False)
+
+        self.host_mail = MailService()
+
+        # Signals from ConfigWindow
+        #self.config_window.signals.run.connect(lambda: self.__run(close_window = True))
+        #self.config_window.signals.exit.connect(self.exit)
+        
+        # Signals from TrayIcon
+        #self.tray_icon.signals.open.connect(self.config_window.show)
+        #self.tray_icon.signals.exit.connect(self.exit)
+
+    def auto_run_check(self):
+        if global_variables.app_configs['auto_run']:
+            self.__run(close_window = False)
+
+    def start(self):
+        #self.tray_icon.show()
+        #self.config_window.show()
+        self.auto_run_check()
+        sys.exit(self.app.exec_())
+
+    def __run_thread(self, status, close_window):
+        '''
+            Run threads for app if status is True
+            After that, the Run button will become Hide button, the config window will closed or not based on close_window
+        '''
+        #self.__dialog.close()
+        if status == True:
+            # Start checking mail box and show notifications
+            self.checking_thread = threading.Thread(target = check_email_thread, args = (self.host_mail, ))
+            self.checking_thread.daemon = True
+            self.checking_thread.start()
+            self.noti_thread = threading.Thread(target = show_notification_thread, args = ())
+            self.noti_thread.daemon = True
+            self.noti_thread.start()
+            #self.config_window.background_setup(close_window)
+        else:
+            self.__show_msg('Cannot login to mail server. Please try again later.')
+
+    def __run(self, close_window):
+        '''
+            Run the app (first run button click or auto-run)
+        '''
+        #self.__dialog.show()
+
+        # create thread and start
+        self.__thread = QThread()
+        self.__target = LoginThread(self.host_mail)
+        self.__target.moveToThread(self.__thread)
+
+        self.__thread.started.connect(self.__target.start)
+
+        self.__target.finished.connect(self.__thread.quit)
+        self.__target.ok.connect(lambda: self.__run_thread(True, close_window))
+        self.__target.fail.connect(lambda: self.__run_thread(False, close_window))
+        
+        self.__thread.start()
+        
+    def __show_msg(self, msg):
+        msg.exec_()
+
+    def exit(self):
+        if self.host_mail:
+            try:
+                logging.log('Logout mail server...')
+                self.host_mail.logout()
+            except:
+                logging.log('Exception raised while loging out')
+                pass
+        
+        logging.log('App closed')
+        logging.save()
+        sys.exit()
