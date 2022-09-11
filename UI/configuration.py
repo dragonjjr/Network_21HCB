ab2e874770c36692901155795eb60bd7163e9908
nakhuong269 <75614193+nakhuong269@users.noreply.github.com>
2022-09-11 14:23:43 +0700

    add show config

--- UI/configuration.py
+++ UI/configuration.py
@@ -1,17 +1,25 @@
+import sys
 from PyQt5.QtWidgets import QWidget, QDialog
 from PyQt5.uic import loadUi
 from PyQt5 import QtGui, QtCore
 from mydialog import MyDialog
+
+sys.path.append('..')
+from helpers import *
+import global_variables  
+
 class Configuration(QWidget):
 	def __init__(self,parent):
 		super(Configuration,self).__init__()
 		loadUi("ui_Configuration.ui",self)
+		self.render_config()
 		self.parent = parent
 		self.btnAddBasic.clicked.connect(self.btnAddBasic_click)
 		self.btnAddAdvanced.clicked.connect(self.btnAddAdvanced_click)
 		self.btnRemoveBasic.clicked.connect(self.btnRemoveBasic_click)
 		self.btnRemoveAdvanced.clicked.connect(self.btnRemoveAdvanced_click)
 		self.btnSave.clicked.connect(self.btnSave_click)
+		
 
 	def btnAddBasic_click(self):
 		dialog = QDialog()
@@ -36,6 +44,19 @@ class Configuration(QWidget):
 	def btnRemoveAdvanced_click(self):
 		#Name listview : lvAdvanced -> self.lvAdvanced
 		print("Remove Advanced")
+
+	def render_config(self):
+		cfg = load_config('../app_configs.yaml')
+		global_variables.app_configs['white_list'] = cfg['white_list']
+		global_variables.app_configs['auto_run'] = cfg['auto_run']
+		self.lvBasic.clear()
+		self.lvAdvanced.clear()
+
+		for mail in global_variables.app_configs['white_list']['basic']:
+			self.lvBasic.addItem(mail)
+		for mail in global_variables.app_configs['white_list']['advanced']:
+			self.lvAdvanced.addItem(mail)
+		self.cbAutoRun.setChecked(global_variables.app_configs['auto_run'])
 
 
 		
