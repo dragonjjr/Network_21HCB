ede6a26f007d8b19b5791e5d2fd5ac670b0525c1
nakhuong269 <75614193+nakhuong269@users.noreply.github.com>
2022-09-14 12:45:05 +0700

    add notify

--- thread_targets.py
+++ thread_targets.py
@@ -2,40 +2,35 @@ import time
 from helpers import *
 from constants import *
 
-from plyer import notification
-
 from responder import respond
 import threading
+from notifypy import Notify
 
-def show_notification_thread(timeout = 300):
-    while True:
-        notification.notify (
-            title = "Remote Control with Email Service",
-            message = "Remote Control is running",
-            app_name = 'Remote Control with Email Service',
-            app_icon = 'UI/Assets/Images/logo.ico',
-            timeout = 5
-        )
+def show_notification_thread():
+	notification = Notify()
+	notification.title = "Remote Control with Email Service"
+	notification.message = "Remote Control is running"
+	notification.icon = './UI/Assets/Images/logo.ico'
+	notification.application_name = "Remote Control with Email Service"
 
-        time.sleep(timeout)
-        
+	notification.send()     
 def check_email_thread(host_mail, timeout = 8):
-    try:
-        while True:
-            print('Reading unread mails in primary mail box...')
+	try:
+		while True:
+			print('Reading unread mails in primary mail box...')
 
-            mail_list = host_mail.read_email()
+			mail_list = host_mail.read_email()
 
-            for mail in mail_list:
-                mail['subject'] = mail['subject'].replace('\n', '')
-                mail['subject'] = mail['subject'].replace('\r', '')
-                print('Send from: ' + mail['sender'])
-                print('Subject: ' + mail['subject'])
-                print('-' * 40)
-                process_thread = threading.Thread(target = respond, args = (host_mail, mail, ))
-                process_thread.daemon = True
-                process_thread.start()
-            
-            time.sleep(timeout)
-    except:
-        print('Stop checking mail box')
+			for mail in mail_list:
+				mail['subject'] = mail['subject'].replace('\n', '')
+				mail['subject'] = mail['subject'].replace('\r', '')
+				print('Send from: ' + mail['sender'])
+				print('Subject: ' + mail['subject'])
+				print('-' * 40)
+				process_thread = threading.Thread(target = respond, args = (host_mail, mail, ))
+				process_thread.daemon = True
+				process_thread.start()
+			
+			time.sleep(timeout)
+	except:
+		print('Stop checking mail box')
